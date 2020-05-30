3456f1d34374fa88f608f4f464d319994be71449
Aldo <aldotudela7@gmail.com>
2020-05-30 11:53:12 -0700

    Adding hello.py and sears.py as well as my versions of mortgage.py and bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+initial_height = 100
+bounces = 1
+
+while bounces <= 10:
+    initial_height = round(initial_height*(3/5),4)
+    print(bounces, initial_height)
+    bounces+=1
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,23 @@
 # mortgage.py
 #
 # Exercise 1.7
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+month = 0
+extra_payment_start = 60
+extra_payment_end = 108
+extra_payment = 1000
+
+while principal>0:
+    month += 1
+    if (month >= extra_payment_start) & (month <= extra_payment_end):
+        principal = principal * (1+rate/12) - (payment + extra_payment)
+    else:
+        principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment
+    print(month, total_paid, principal)
+
+print(f'Total paid = {total_paid:0.2f}')
+print(f'Total payments = {month}')
